c3f1510f3ea83423d2db7135b1ede26b0c94f763
Justin Ross <jross@apache.org>
2017-12-23 14:00:19 -0800

    Likely values for the link and page ignore vars

--- input/.transom/config.py
+++ input/.transom/config.py
@@ -134,3 +134,17 @@ in reprehenderit in voluptate velit esse cillum dolore eu fugiat
 nulla pariatur. Excepteur sint occaecat cupidatat non proident,
 sunt in culpa qui officia deserunt mollit anim id est laborum.
 """
+
+ignore_links += [
+    "releases/qpid-*/java-broker/book/*",
+    "releases/qpid-broker-j*/book/*"
+    "releases/qpid-*/jms-client-0-8/book/*",
+    "releases/qpid-java-*/jms-client-0-10/book/images/callouts/*",
+]
+
+ignore_pages += [
+    "releases/qpid-cpp-*/messaging-api/cpp/api/*",
+    "releases/qpid-python-*/messaging-api/api/*",
+    "releases/qpid-proton-*/proton/*/api/*",
+    "releases/qpid-proton-j-*/api/*",
+]
