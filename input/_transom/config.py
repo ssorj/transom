5bccbf4cc608842b7ffe0becd16a46254879026d
Justin Ross <jross@apache.org>
2019-11-21 16:03:32 -0500

    Add a lipsum function

--- input/_transom/config.py
+++ input/_transom/config.py
@@ -125,16 +125,6 @@ def travis_ci_badge(party_key, job_key, branch="master"):
 
     return "<a href=\"{}\"><img src=\"{}\" height=\"20\"/></a>".format(job_url, image_url)
 
-lipsum = """
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-enim ad minim veniam, quis nostrud exercitation ullamco laboris
-nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-sunt in culpa qui officia deserunt mollit anim id est laborum.
-"""
-
 ignored_links += [
     "/releases/qpid-*/java-broker/book/*",
     "/releases/qpid-broker-j-*/book/*"
